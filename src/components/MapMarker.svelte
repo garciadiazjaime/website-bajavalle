57ecd2296f97de9b8ad01bb9297c7e27b7ed974b
jaime <garciadiazjaime@gmail.com>
2021-09-20 17:49:53 -0500

    feat: custom marker

--- src/components/MapMarker.svelte
+++ src/components/MapMarker.svelte
@@ -1,3 +1,13 @@
+<style>
+	:global(.marker) {
+		background-image: url('/mapbox-icon.png');
+		background-size: cover;
+		width: 50px;
+		height: 50px;
+		border-radius: 50%;
+		cursor: pointer;
+	}
+</style>
 <script>
 	import { getContext } from 'svelte';
 	import { mapbox, key } from '../support/mapbox.js';
@@ -12,7 +22,10 @@
 	const popup = new mapbox.Popup({ offset: 25 })
 		.setText(label);
 
-	const marker = new mapbox.Marker()
+	const el = document.createElement('div');
+  el.className = 'marker';
+
+	const marker = new mapbox.Marker(el)
 		.setLngLat([lon, lat])
 		.setPopup(popup)
 		.addTo(map);

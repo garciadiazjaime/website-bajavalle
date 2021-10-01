1af867f11b5229be77445cdefbf1db5e4f6a76f1
jaime <garciadiazjaime@gmail.com>
2021-09-30 21:20:30 -0500

    feat: show imgs instead of map

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,32 +1,74 @@
-<script>
-	import Map from '../components/MapHOC.svelte';
+<style>
+	.container {
+		max-width: 1120px;
+		margin: 0 auto;
+	}
 
-	const center = {
-		lat: 32.028019,
-		lon: -116.6330805,
-		zoom: 9,
+	.gallery {
+		display: flex;		
 	}
-	const items = {
-		"baja-valle-retreat": {
-			title: "Baja Valley Retreat",
-			lat: 32.028019,
-			lon: -116.6330805,
-			images: [
-				'house/conejo/front-night.jpeg',
-				'house/conejo/west-side.jpeg',
-				'house/conejo/east-side.jpeg',
-				'house/conejo/entrance.jpeg',
-				'house/conejo/front-inside.jpeg',
-				'house/conejo/garden.jpeg',
-				'house/conejo/sky.jpeg',
-				'house/conejo/terrace.jpeg',
-			]
-		}
+
+	.gallery-cover {
+		margin-right: 8px;
 	}
-</script>
+
+	img {
+		object-fit: cover;
+		vertical-align: bottom;
+	}
+
+	.gallery-cover img {
+		height: 568px;
+		width: 560px;
+	}
+
+	.gallery-items img {
+		height: 280px;
+		width: 272px;
+	}
+
+	.gallery-row {
+		display: flex;
+	}
+
+	.gallery-row img:first-of-type {
+		margin-right: 8px;
+	}
+
+	.gallery-row:first-of-type img{
+		margin-bottom: 8px;
+	}
+
+	.left-border {
+		border-radius: 6px 0 0 6px;
+	}
+</style>
+
 
 <svelte:head>
 	<title>Baja Valley Retreat</title>
 </svelte:head>
 
-<Map items={items} center={center} />
+<div class="container">
+
+	<h1>Baja Valley Retreat</h1>
+	<h2>Valle de Guadalupe, Baja California, Mexico</h2>
+	
+	<div class="gallery">
+		<div class="gallery-cover">
+			<img src="house/conejo/front-night.jpeg" alt="" class="left-border">
+		</div>
+		<div class="gallery-items">
+			<div class="gallery-row">
+				<img src="house/conejo/west-side.jpeg" alt="">
+				<img src="house/conejo/east-side.jpeg" alt="">
+			</div>
+			
+			<div class="gallery-row">
+				<img src="house/conejo/entrance.jpeg" alt="">
+				<img src="house/conejo/front-inside.jpeg" alt="">
+			</div>
+		</div>
+	</div>
+	
+</div>
